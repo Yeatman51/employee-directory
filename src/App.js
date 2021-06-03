69f90cdf78d9a9129a5b9cfb5c2f63eca83ec479
Jon Yeatman <jonlyeatman@gmail.com>
2021-06-02 21:18:23 -0400

    Header/ footer components created. working with API data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Header from "./components/Header";
+import API from "./utlis/API"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Test</h1>
+      <API />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header className="bg-info bg-gradient text-dark">
+        <br/>
+        <h1>Employee Directory</h1>
+        <br/>
+    </header>
+  );
+}
+
+export default Header;
