5cccf7ed2769ff5a9f25d112391a44fad9e3ddbd
Jon Yeatman <jonlyeatman@gmail.com>
2021-06-09 20:56:01 -0400

    New results component

---
+++ src/components/Results/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import ReactDOM from 'react-dom';
+import API from "../../utlis/API";
+
+
+function Results(props) {
+  return (
+    <div>
+        hello {API}
+    </div>
+  );
+}
+
+export default Results;
